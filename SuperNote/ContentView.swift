e4c6ff9bda55b7c9fcdf95b265b5b07c9ec5c170
Caiyuzhen <caiyuzhen00@gmail.com>
2023-08-01 00:24:01 +0800

    test

--- SuperNote/ContentView.swift
+++ SuperNote/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world111!")
+            Text("Hello, world222!")
         }
         .padding()
     }
