d94ed89c03df2d348208b0bd726fffc7ae14a7f2
Caiyuzhen <caiyuzhen00@gmail.com>
2023-08-01 00:13:26 +0800

    Initial Commit

---
+++ SuperNote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SuperNote
+//
+//  Created by ai C on 2023/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperNote/SuperNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperNoteApp.swift
+//  SuperNote
+//
+//  Created by ai C on 2023/8/1.
+//
+
+import SwiftUI
+
+@main
+struct SuperNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
