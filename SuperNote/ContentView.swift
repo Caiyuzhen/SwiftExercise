d3946bb2181455d652adee606eb8e80273188165
Caiyuzhen <caiyuzhen00@gmail.com>
2023-08-04 01:34:01 +0800

    improveUI

--- SuperNote/ContentView.swift
+++ SuperNote/ContentView.swift
@@ -10,7 +10,7 @@ struct ContentView: View {
     let customColor = Color(red: 0.28, green: 0.22, blue: 0.9, opacity: 1) //自定义颜色
     
     // 👇渲染 UI
-    var body: some View {
+    var body: some View { //some 表示不透明【类型】
         VStack(spacing: 10) {//垂直排列(类似 flex)
             Text("今天吃什么？")
                 .font(.largeTitle)
@@ -19,7 +19,7 @@ struct ContentView: View {
             
             
             Image("hambuger")
-                .resizable()
+                .resizable() // 🚀（调整器的位置很重要！因为每一行都会返回自己的 View）
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 240.0, height: 240.0)
                 
@@ -34,16 +34,38 @@ struct ContentView: View {
                     .padding(.vertical, 10)
                     .bold()
 					.foregroundColor(customColor)
+                    .id(selectedFood) // 🔥设定了 id 后, Swift UI 就会明确转场的是不同的对象效果为淡入淡出
+//                    .transition(.scale.combined(with: .slide)) //组合动画
+                    .transition(.asymmetric( //🔥分别设置进场、离场动画的动画曲线(需要有设置 id，不然 Swift 无法识别）
+                        insertion:
+                            .opacity
+                            .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                        removal:
+                            .opacity.animation(
+                            .easeInOut(duration: 0.4))))
             }
+            
+            // 🚀👇下面两种实现方式会产生不同的动画
+            selectedFood != .none ? Color.pink : Color.blue //【运算值的方式】, 会被判断为【同一个元素 - 同一个 View】, 因此会变成补帧的【形变动画】
+            
+//            if selectedFood != .none { //【条件值的方式】, 会被判断为【不同的元素 - 不同的 View】, 就会发生【转场动画】,因为 if 语句会被包装成 EitherView
+//                Color.pink
+//            } else {
+//                Color.blue
+//            }
             
             
             // 👇用带 label 的 Button 可以调整按钮的样式
             Button(role: .none) {
-                selectedFood = food.shuffled().filter { $0 != selectedFood }.first //shuffled() 利用原始数组返回一个新的数组，其中包含原始数组中的元素，但顺序是随机的； $0 ! 表示过滤掉跟当前一样的元素，随机抽取下一个非当前元素的元素
+                withAnimation { //👈这个加了后会只影响当前这个元素, 单独让它有动画, 可以设置有动画的时机
+                    selectedFood = food.shuffled().filter { $0 != selectedFood }.first //shuffled() 利用原始数组返回一个新的数组，其中包含原始数组中的元素，但顺序是随机的； $0 ! 表示过滤掉跟当前一样的元素，随机抽取下一个非当前元素的元素
+                }
             } label: {
                 Text(selectedFood == .none ? "告诉我" : "手气不错")
+                    .animation(.none, value: selectedFood) //删除文字的渐变动画（系统默认效果）
                     .foregroundColor(.white)
                     .frame(width: 180, height: 38, alignment: .center) // 改变按钮宽度
+                    .transformEffect(.init(translationX: 0, y: 0)) //明确文字的位置（ 涉及到了 Swift 内置动画的原理, Swift 内置了转场动画跟位移动画, 避免让文字元素产生上下移动的效果, 相当于不让位置变化）
             }
                 .buttonStyle(.borderedProminent)
                 .cornerRadius(12)
@@ -64,9 +86,10 @@ struct ContentView: View {
 
             
         }
+        
+            .frame(maxWidth: .infinity, maxHeight: .infinity)//背景无限延伸 （调整器的位置很重要！）
             .padding().opacity(0.8)
             .animation(.easeInOut(duration: 0.3), value: selectedFood) //⚡️这个动画要放在 VStack 身上，是因为要在 VStack 开始出现时就开始观察动画，动画的时间跟变化速率 、变化的对象（比如食物文字发生变化，就执行动画）
-            .frame(maxWidth: .infinity, maxHeight: .infinity)//背景无线延伸
             .background(Color(.secondarySystemBackground))
     }
     
